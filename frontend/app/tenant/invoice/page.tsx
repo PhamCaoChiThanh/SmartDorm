8e2289c89479e62d6534999f8da5e3944f70b9d9
PhamCaoChiThanh <chithanhphamj@gmail.com>
2026-07-04 22:36:01 +0700

    Update MoMo QR code link to use me.momo.vn domain instead of nhantien

--- frontend/app/tenant/invoice/page.tsx
+++ frontend/app/tenant/invoice/page.tsx
@@ -650,7 +650,7 @@ export default function TenantInvoice() {
                     </div>
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
-                      src={`https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${encodeURIComponent(`https://nhantien.momo.vn/0704569016`)}`}
+                      src={`https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${encodeURIComponent(`https://me.momo.vn/0704569016`)}`}
                       alt="MoMo QR Code"
                       className="w-48 h-48 object-contain border rounded-xl shadow-xs"
                     />
