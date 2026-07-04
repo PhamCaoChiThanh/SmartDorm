e9a2e0ec26d2767c2a1fd0d597cbe06ab8912cf1
PhamCaoChiThanh <chithanhphamj@gmail.com>
2026-07-04 22:39:50 +0700

    Configure bank code MOMO with phone number 0704569016 in VietQR

--- frontend/app/tenant/invoice/page.tsx
+++ frontend/app/tenant/invoice/page.tsx
@@ -650,7 +650,7 @@ export default function TenantInvoice() {
                     </div>
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
-                      src={`https://img.vietqr.io/image/VietCapitalBank-0704569016-compact2.png?amount=${total}&addInfo=${encodeURIComponent(qrTransferContent)}&accountName=${encodeURIComponent("PHAM CAO CHI THANH")}`}
+                      src={`https://img.vietqr.io/image/MOMO-0704569016-compact2.png?amount=${total}&addInfo=${encodeURIComponent(qrTransferContent)}&accountName=${encodeURIComponent("PHAM CAO CHI THANH")}`}
                       alt="MoMo QR Code"
                       className="w-48 h-48 object-contain border rounded-xl shadow-xs"
                     />
